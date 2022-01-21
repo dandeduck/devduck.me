5cb7fdbc8efc81bdfed7d4c2d3071c053a0798ef
dandeduck <danielmkhlv@gmail.com>
2022-01-22 01:15:10 +0200

    Added basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import duck from './duck.svg';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className='Header'>
+      <a className='Header-link yelloNeon' href='\'>about me</a>
+      <a href='\'>
+        <img src={duck} className='neonDuck' alt="duck"/>
+      </a>
+      <a className='Header-link purpleNeon' href='\'>daily</a>
+      <a className='Header-link brokenNeon' href='\'>my work</a>
+    </header>
+  );
+}
