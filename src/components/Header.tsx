3326d9613906497332dc40a22fd4bedfabdb3f0f
dandeduck <danielmkhlv@gmail.com>
2022-01-23 20:36:55 +0200

    Added header hover effects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import './Header.css';
 export default function Header() {
   return (
     <header className='Header'>
-      <a className='Header-link yelloNeon' href='/'>about me</a>
       <a href='/'>
         <img src={duck} className='neonDuck' alt="duck"/>
       </a>
-      <a className='Header-link purpleNeon' href='/'>daily</a>
-      <a className='Header-link brokenNeon' href='/'>my work</a>
-      <a className='Header-link contactLink' href='/'>contact</a>
+      <a className='Header-link' href='/'>about me</a>
+      <a className='Header-link' href='/'>daily</a>
+      <a className='Header-link' href='/'>my work</a>
+      <a className='Header-link' href='/'>contact</a>
     </header>
   );
 }
