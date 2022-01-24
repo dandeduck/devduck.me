f8e405fdca4f198b44e8aeb6f3cd41e01aeda9aa
dandeduck <danielmkhlv@gmail.com>
2022-01-24 15:04:56 +0200

    added easter egg and b=a bit of responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,14 @@ import './Header.css';
 export default function Header() {
   return (
     <div className='navContainer'>
-      <nav className='Header'>
-        <a href='/'>
+      <nav className='navbar'>
+        <div className='duck'>
           <img src={duck} className='neonDuck' alt="duck"/>
-        </a>
-        <a className='Header-link underline' href='/'>about me</a>
-        <a className='Header-link underline' href='/'>daily</a>
-        <a className='Header-link underline' href='/'>my work</a>
-        <a className='Header-link underline' href='/'>contact</a>
+        </div>
+        <a className='navbar-link underline' href='/'>about me</a>
+        <a className='navbar-link underline' href='/'>daily</a>
+        <a className='navbar-link underline' href='/'>my work</a>
+        <a className='navbar-link underline' href='/'>contact</a>
       </nav>
     </div>
   );
