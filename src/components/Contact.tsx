868694a8e1b60af4d8e382d6fdb2f40447e6b643
dandeduck <danielmkhlv@gmail.com>
2022-01-25 12:29:44 +0200

    redesigned reach me at window

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,32 +1,41 @@
+import duck from './duck.svg';
 import './Contact.css';
 
 export default function Header() {
   return (
     <div className='contact'>
       <h1 className='heading'>lets talk!</h1>
-      <div className='inputs'>
-        <div className='entry'>
-          <label htmlFor='name'><code className='text'>&#60;name&#62;</code></label>
-          <input id='name' className='field'></input>
-          <label htmlFor='name'><code className='text'>&#60;/name&#62;</code></label>
+      <div className='contact-container'>
+        <form className='contact-form'>
+          <span className='text third-heading'>&#60;send me a message/&#62;</span>
+          <div className='inputs'>
+            <div className='entry'>
+              <label htmlFor='name'><code className='text'>&#60;name&#62;</code></label>
+              <input id='name' className='field'></input>
+              <label htmlFor='name'><code className='text'>&#60;/name&#62;</code></label>
+            </div>
+            <div className='entry'>
+              <label htmlFor='email'><code className='text'>&#60;email&#62;</code></label>
+              <input id='email' className='field'></input>
+              <label htmlFor='email'><code className='text'>&#60;/email&#62;</code></label>
+            </div>
+            <div className='entry'>
+              <label htmlFor='message'><code className='text'>&#60;message&#62;</code></label>
+              <textarea id='message' className='big field'></textarea>
+              <label htmlFor='message'><code className='text'>&#60;/message&#62;</code></label>
+            </div>
+            <button type='submit' className='submit'>send</button>
+          </div>
+        </form>
+        <div className='info text'>
+          <div className='speach'>
+            <img src={duck} className='neonDuck talking-duck' alt="duck"/>
+            <span className='block second-heading'>"You can also reach me at"</span>
+          </div>
+          <span className='block'>danielmkhlv@gmail.com</span>
+          <a href='https://github.com/dandeduck' className='block'>github.com/dandeduck</a>
+          <span className='block'>+972 587976897</span>
         </div>
-        <div className='entry'>
-          <label htmlFor='email'><code className='text'>&#60;email&#62;</code></label>
-          <input id='email' className='field'></input>
-          <label htmlFor='email'><code className='text'>&#60;/email&#62;</code></label>
-        </div>
-        <div className='entry'>
-          <label htmlFor='message'><code className='text'>&#60;message&#62;</code></label>
-          <textarea id='message' className='big field'></textarea>
-          <label htmlFor='message'><code className='text'>&#60;/message&#62;</code></label>
-        </div>
-        <button type='submit' className='submit'>send</button>
-      </div>
-      <div className='info text'>
-        <span className='small second-heading'>You can also reach me at:</span>
-        <a href='https://github.com/dandeduck' className='small'>github.com/dandeduck</a>
-        <span className='small'>danielmkhlv@gmail.com</span>
-        <span className='small'>+972 587976897</span>
       </div>
     </div>
   );
