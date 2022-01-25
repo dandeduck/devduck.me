59bb497f18f626967128cdf86b7de5b6d9e22209
dandeduck <danielmkhlv@gmail.com>
2022-01-25 10:56:29 +0200

    Updated responsiveness

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,24 +8,26 @@ export default function Header() {
         <div className='entry'>
           <label htmlFor='name'><code className='text'>&#60;name&#62;</code></label>
           <input id='name' className='field'></input>
-          <label htmlFor='name'><code className='text'>&#60;name/&#62;</code></label>
+          <label htmlFor='name'><code className='text'>&#60;/name&#62;</code></label>
         </div>
-        <div className='entry hidden'>
+        <div className='entry'>
           <label htmlFor='email'><code className='text'>&#60;email&#62;</code></label>
           <input id='email' className='field'></input>
-          <label htmlFor='email'><code className='text'>&#60;email/&#62;</code></label>
+          <label htmlFor='email'><code className='text'>&#60;/email&#62;</code></label>
         </div>
-        <div className='entry hidden'>
+        <div className='entry'>
           <label htmlFor='message'><code className='text'>&#60;message&#62;</code></label>
           <textarea id='message' className='big field'></textarea>
-          <label htmlFor='message'><code className='text'>&#60;message/&#62;</code></label>
+          <label htmlFor='message'><code className='text'>&#60;/message&#62;</code></label>
         </div>
         <button type='submit' className='submit'>send</button>
       </div>
-      <span className='small text second-heading'>You can also reach me at:</span>
-      <a href='https://github.com/dandeduck' className='small text'>github.com/dandeduck</a>
-      <span className='small text'>danielmkhlv@gmail.com</span>
-      <span className='small text'>+972 587976897</span>
+      <div className='info text'>
+        <span className='small second-heading'>You can also reach me at:</span>
+        <a href='https://github.com/dandeduck' className='small'>github.com/dandeduck</a>
+        <span className='small'>danielmkhlv@gmail.com</span>
+        <span className='small'>+972 587976897</span>
+      </div>
     </div>
   );
 }
