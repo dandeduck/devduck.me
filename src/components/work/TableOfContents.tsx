3cf17fc058a968125d6f5f318c64c0119dc004a4
dandeduck <danielmkhlv@gmail.com>
2022-01-26 09:00:59 +0200

    implemented table of contents

--- src/components/work/TableOfContents.tsx
+++ src/components/work/TableOfContents.tsx
@@ -1,9 +1,17 @@
 import './TableOfContents.css';
 
-export default function TableOfContents(props: {name: string}) {
+export default function TableOfContents(props: {title: string, sections: string[]}) {
+    const onClick = (e: React.MouseEvent, id: string) => {
+        e.preventDefault();
+        document.getElementById(id)?.scrollIntoView({behavior: 'smooth', block: "start"});
+    };
+
     return (
         <div className='table'>
-            <p className='name'>{props.name}</p>
+            <p className='title code-look'>{props.title}</p>
+            <div className='sections'>
+                {props.sections.map(section => <a href='/' onClick={(e) => onClick(e, section)} className='section code-look'>{section}</a>)}
+            </div>
         </div>
     );
 }
--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -7,16 +7,29 @@ export default function Work() {
   return (
     <div className='work'>
       <Page html={<FirstPage/>}/>
-      <Page html={<FirstPage/>} hasNext={false}/>
+      <Page html={<GameDevelopment/>} hasNext={false}/>
     </div>
   );
 }
 
 const FirstPage = () => {
   return (
-    <div>
+    <div id='first-page'>
       <Quote sentence='Do or do not. There is no try.' source='Grand Master Yoda'/>
-      <TableOfContents name='categories'/>
+      <div className='table-container'>
+        <TableOfContents title='What would you like to see first' sections={['game development', 'web development', 'robotics', 'other']}/>
+      </div>
+    </div>
+  );
+}
+
+const GameDevelopment = () => {
+  return (
+    <div id='game development'>
+      <Quote sentence='Do or do not. There is no try.' source='Grand Master Yoda'/>
+      <div className='table-container'>
+        <TableOfContents title='What would you like to see first' sections={['game development', 'web development', 'robotics', 'other']}/>
+      </div>
     </div>
   );
 }
