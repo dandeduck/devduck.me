8d6f17d99d9bab68a34bb390e9467cc28eca1571
dandeduck <danielmkhlv@gmail.com>
2022-01-26 20:56:22 +0200

    Removed other page, fixed project and changed table styling a bit

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,13 +1,11 @@
 import Page from './Page';
 import WorkIntro from './pages/WorkIntro';
-import OtherPage from './pages/OtherPage';
 import './Work.css'
 
 export default function Work() {
   return (
     <div className='work'>
       <Page html={<WorkIntro/>}/>
-      <Page html={<OtherPage/>} hasNext={false}/>
     </div>
   );
 }
--- src/components/work/pages/Project.tsx
+++ src/components/work/pages/Project.tsx
@@ -1,13 +1,13 @@
-import icon from '../../assets/github.svg';
+import github from '../../assets/github.svg';
 import './Project.css';
 
 export default function Project(props: {name: string, link: string, description: string, technologies: string[]}) {
   return (
-    <a href={props.link} className='project-link'>
+    <a href={props.link} className='project-link' target="_blank" rel="noreferrer">
       <div className='project-block'>
-        <p className='project-name'>{props.name}<img className='project-icon' src={icon} alt='external link'></img></p>
+        <p className='project-name'>{props.name}<img className='project-icon' src={github} alt='external link'></img></p>
         <p className='project-description'>{props.description}</p>
-        {props.technologies.map(tech => <span className='project-technology'>{tech}</span>)}
+        <div className='project-technologies'>{props.technologies.map(tech => <span className='project-technology'>{tech}</span>)}</div>
       </div>
     </a>
   );
