4516d09abf893160205891046d02fc1061a0fcdb
dandeduck <danielmkhlv@gmail.com>
2022-01-29 13:30:34 +0200

    Added centering to work, again...

--- src/components/work/pages/GameDevelopment.tsx
+++ src/components/work/pages/GameDevelopment.tsx
@@ -5,21 +5,23 @@ import './GameDevelopment.css';
 export default function GameDevelopment() {
   return (
     <div id='game development' className='game-development code-look'>
-        <h1>game development</h1>
-        <div className='game-development-container'>
-          <div className='game-development-main'>
-            <p className='game-development-text'>I first began my game development journey by learning Unity through the internet, by then I already had a number of years of experience in software development in general.</p>
-            <p className='game-development-text'>I got my <i>professional</i> experience by working on simulations and educational tools for the IAF.</p> 
-            <p className='game-development-text'>In total, I have 1 year of experience with one <a href='https://github.com/dandeduck/asteroids-unlimited' className='link' target="_blank" rel="noreferrer">indie game</a> under development and one prototype game.</p>
-            <div className='game-development-projects'>
-              <Project name='asteroids UNLIMITED' link='https://github.com/dandeduck/asteroids-unlimited' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'AI', 'Multiplayer']} />
-              <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
-              <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
-              <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
+      <div className='game-development-centered-container'>
+          <h1>game development</h1>
+          <div className='game-development-container'>
+            <div className='game-development-main'>
+              <p className='game-development-text'>I first began my game development journey by learning Unity through the internet, by then I already had a number of years of experience in software development in general.</p>
+              <p className='game-development-text'>I got my <i>professional</i> experience by working on simulations and educational tools for the IAF.</p> 
+              <p className='game-development-text'>In total, I have 1 year of experience with one <a href='https://github.com/dandeduck/asteroids-unlimited' className='link' target="_blank" rel="noreferrer">indie game</a> under development and one prototype game.</p>
+              <div className='game-development-projects'>
+                <Project name='asteroids UNLIMITED' link='https://github.com/dandeduck/asteroids-unlimited' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'AI', 'Multiplayer']} />
+                <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
+                <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
+                <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
+              </div>
             </div>
+            <LocationTeller locations={['start', 'game development', 'web development', 'robotics']} currentLocation='game development'/>
           </div>
-          <LocationTeller locations={['start', 'game development', 'web development', 'robotics']} currentLocation='game development'/>
-        </div>
+      </div>
     </div>
   );
 }
--- src/components/work/pages/WorkIntro.tsx
+++ src/components/work/pages/WorkIntro.tsx
@@ -5,8 +5,10 @@ import './WorkIntro.css';
 export default function WorkIntro() {
   return (
     <div id='start'>
-      <Quote sentence='Do or do not. There is no try.' source='Grand Master Yoda'/>
-      <TableOfContents title='What would you like to see first?' sections={['game development', 'web fullstack', 'robotics']}/>
+      <div className='work-intro-container'>
+        <Quote sentence='Do or do not. There is no try.' source='Grand Master Yoda'/>
+        <TableOfContents title='What would you like to see first?' sections={['game development', 'web fullstack', 'robotics']}/>
+      </div>
     </div>
   );
 }
