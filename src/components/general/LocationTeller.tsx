1eb9ae6d0afdcb0f4c032a6b51ef53c234955363
dandeduck <danielmkhlv@gmail.com>
2022-01-29 18:20:26 +0200

    Removed jumping to things functionality

--- src/components/general/LocationTeller.tsx
+++ src/components/general/LocationTeller.tsx
@@ -1,14 +1,9 @@
 import './LocationTeller.css';
 
 export default function LocationTeller(props: {locations: string[], currentLocation: string}) {
-  const onClick = (e: React.MouseEvent, id: string) => {
-    e.preventDefault();
-    document.getElementById(id)?.scrollIntoView({behavior: 'smooth', block: "start"});
-  };
-
   return (
     <div className='location-teller'>
-      {props.locations.map((location, i) => <a href='/' key={i} className={props.currentLocation === location ? 'selected' : 'unselected'} onClick={e => onClick(e, location)}>{location}</a>)}
+      {props.locations.map((location, i) => <span key={i} className={props.currentLocation === location ? 'selected' : 'unselected'}>{location}</span>)}
     </div>
   );
 }
--- src/components/work/pages/WorkIntro.tsx
+++ src/components/work/pages/WorkIntro.tsx
@@ -1,4 +1,3 @@
-import TableOfContents from '../TableOfContents';
 import Quote from '../../general/Quote';
 import arrow from '../../assets/arrowDown.svg';
 import './WorkIntro.css';
@@ -8,7 +7,6 @@ export default function WorkIntro() {
     <div id='start'>
       <div className='work-intro-container'>
         <Quote sentence='Do or do not. There is no try.' source='Grand Master Yoda'/>
-        <TableOfContents title='What would you like to see first?' sections={['game development', 'web fullstack', 'robotics']}/>
       </div>
       <img src={arrow} alt='down'></img>
     </div>
