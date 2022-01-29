3f3763d62ba74c4e068a87d35b78d4035ab8e702
dandeduck <danielmkhlv@gmail.com>
2022-01-29 18:15:23 +0200

    Added description to added projects

--- src/components/work/pages/GameDevelopment.tsx
+++ src/components/work/pages/GameDevelopment.tsx
@@ -13,8 +13,8 @@ export default function GameDevelopment() {
               <p className='game-development-text'>I got my <i>professional</i> experience by working on simulations and educational tools for the IAF.</p> 
               <p className='game-development-text'>In total, I have 1 year of experience with one <a href='https://github.com/dandeduck/asteroids-unlimited' className='link' target="_blank" rel="noreferrer">indie game</a> under development and one prototype game.</p>
               <div className='game-development-projects'>
-                <Project name='asteroids UNLIMITED' link='https://github.com/dandeduck/asteroids-unlimited' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'AI', 'Multiplayer']} />
-                <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis dictum magna ut mattis. Nulla facilisi. Morbi ut neque orci. Suspendisse tortor metus, su' technologies={['Unity', 'RPG Systems']} />
+                <Project name='asteroids UNLIMITED' link='https://github.com/dandeduck/asteroids-unlimited' description='A space themed RTS style game, with ship combat, building, movement and capturing mechanics. Developed with help in modeling and visual design by my friends.' technologies={['Unity', 'AI', 'Multiplayer']} />
+                <Project name='savanna' link='https://github.com/dandeduck/savanna' description='Prototype project, developed to learn the ins and outs of the Unity game engine and different approaches to RPG mechanics. Like items, crafting, world generation and others.' technologies={['Unity', 'RPG Systems']} />
               </div>
             </div>
             <LocationTeller locations={['start', 'game development', 'web fullstack', 'robotics']} currentLocation='game development'/>
