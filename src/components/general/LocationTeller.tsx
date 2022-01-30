bb373646d4c58d879eb778b5838e8392b226c5f8
dandeduck <danielmkhlv@gmail.com>
2022-01-30 13:18:28 +0200

    Added three

--- src/components/general/LocationTeller.tsx
+++ src/components/general/LocationTeller.tsx
@@ -1,9 +1,14 @@
 import './LocationTeller.css';
 
 export default function LocationTeller(props: {locations: string[], currentLocation: string}) {
+  const onClick = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    document.getElementById(id)?.scrollIntoView({behavior: 'smooth', block: "start"});
+};
+
   return (
     <div className='location-teller'>
-      {props.locations.map((location, i) => <span key={i} className={props.currentLocation === location ? 'selected' : 'unselected'}>{location}</span>)}
+      {props.locations.map((location, i) => <a key={i} href='/' onClick={e => onClick(e, location)} className={props.currentLocation === location ? 'selected' : 'unselected'}>{location}</a>)}
     </div>
   );
 }
