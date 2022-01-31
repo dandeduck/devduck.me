2dcae9e562dcd747a0178c3a85a8ec46a6687fdd
dandeduck <danielmkhlv@gmail.com>
2022-01-31 18:34:45 +0200

    extracted animations and three to separate functions

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -21,119 +21,28 @@ export default function Work() {
   window.addEventListener('scroll', onScroll);
 
   useEffect(() => {
-    const css3DRenderer = new CSS3DRenderer({
-      element: document.querySelector('#css3d') as HTMLElement
-    });
-    css3DRenderer.setSize(window.innerWidth, window.innerHeight);
+    const three = initThree();
+    const camera = three.camera;
+    const scene = three.scene;
 
-    const webglRenderer = new THREE.WebGLRenderer({
-      canvas: document.querySelector('#webgl') as HTMLElement,
-      alpha: true,
-    });
-    webglRenderer.setSize(window.innerWidth, window.innerHeight);
-    webglRenderer.setPixelRatio(window.devicePixelRatio);
-
-    const camera = new THREE.PerspectiveCamera(50, 2, 1, 5000);
-    camera.position.set(0, 0, 8.5);
-
-    const scene = new THREE.Scene();
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x0D1117 } );
-    const cube = new THREE.Mesh( geometry, material );
-    cube.scale.set(15, 8, 1)
-    scene.add( cube );
     
-    function animate() {
-      webglRenderer.render(scene, camera);
-      css3DRenderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    }
-    requestAnimationFrame(animate);
-    
-    gsap.to(camera.position, {
-      z: 12,
-      scrollTrigger: {
-        trigger: '.game-development',
-        start: "top top",
-        end: "bottom top",
-        scrub: true
-      }
-    });
-    
-    gsap.to(camera.rotation, {
-      y: 0.5,
-      scrollTrigger: {
-        trigger: '.game-development',
-        start: "bottom center",
-        end: "bottom top",
-        scrub: true
-      }
-    });
-
     const gameDev = createCSS3DObject(<GameDevelopment/>);
-    enterThree('.game-development', scene, gameDev);
-    // exitThree('#robotics', scene, gameDev);
+    setThreeEnter('.game-development', scene, gameDev);
+    zoomOutAnimation(camera, '.game-development');
+    zoomBackInAnimation(camera);
 
-    gsap.to(camera.position, {
-      immediateRender: false,
-      z: 15,
-      scrollTrigger: {
-        trigger: '#second-dummy',
-        start: "top bottom",
-        end: "bottom bottom",
-        scrub: true
-      }
-    });
-
-    gsap.to(camera.position, {
-      immediateRender: false,
-      z: 8.5,
-      scrollTrigger: {
-        trigger: '#first-section-ending',
-        start: "top bottom",
-        end: "bottom bottom",
-        scrub: true,
-      }
-    });
-
-    gsap.to(camera.rotation, {
-      immediateRender: false,
-      y: 0,
-      scrollTrigger: {
-        trigger: '#first-section-ending',
-        start: "top bottom",
-        end: "bottom bottom",
-        scrub: true
-      }
-    });
-
-    gsap.fromTo('.threeContainer', 
-    {
-      immediateRender: false,
-      top: 0
-    },
-    {
-      position: 'absolute',
-      top: 'calc(100% - 100vh)',
-      scrollTrigger: {
-        trigger: '#first-section-ending',
-        start: "bottom bottom",
-        end: "bottom bottom",
-        scrub: true
-      }
-    });
+    
   }, []);
 
   return (
     <div className='work'>
       <WorkIntro/>
-      <section className='first'>
+      <GameDevelopment/>
+      <section className='three'>
         <div className='threeContainer'>
           <canvas id='webgl'></canvas>
           <div id='css3d'></div>
         </div>
-        <GameDevelopment/>
         <div className='dummy'></div>
         <div id='second-dummy' className='dummy'></div>
         <div id='first-section-ending' className='dummy'></div>
@@ -143,12 +52,121 @@ export default function Work() {
   );
 }
 
+function initThree() {
+  const css3DRenderer = new CSS3DRenderer({
+    element: document.querySelector('#css3d') as HTMLElement
+  });
+  css3DRenderer.setSize(window.innerWidth, window.innerHeight);
+
+  const webglRenderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#webgl') as HTMLElement,
+    alpha: true,
+  });
+  webglRenderer.setSize(window.innerWidth, window.innerHeight);
+  webglRenderer.setPixelRatio(window.devicePixelRatio);
+
+  const camera = new THREE.PerspectiveCamera(50, 2, 1, 5000);
+  camera.position.set(0, 0, 8.5);
+
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial( { color: 0x0D1117 } );
+  const cube = new THREE.Mesh( geometry, material );
+  cube.scale.set(15, 8, 1)
+  scene.add( cube );
+  
+  function animate() {
+    webglRenderer.render(scene, camera);
+    css3DRenderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+
+  return {
+    scene: scene,
+    camera: camera
+  };
+}
+
+function zoomOutAnimation(camera: THREE.Camera, trigger: string) {
+  gsap.to(camera.position, {
+    z: 12,
+    scrollTrigger: {
+      trigger: trigger,
+      start: "top top",
+      end: "bottom top",
+      scrub: true
+    }
+  });
+  
+  gsap.to(camera.rotation, {
+    y: 0.5,
+    scrollTrigger: {
+      trigger: trigger,
+      start: "bottom center",
+      end: "bottom top",
+      scrub: true
+    }
+  });
+  gsap.to(camera.position, {
+    immediateRender: false,
+    z: 15,
+    scrollTrigger: {
+      trigger: '#second-dummy',
+      start: "top bottom",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
+}
+
+function zoomBackInAnimation(camera: THREE.Camera) {
+  gsap.to(camera.position, {
+    immediateRender: false,
+    z: 8.5,
+    scrollTrigger: {
+      trigger: '#first-section-ending',
+      start: "top bottom",
+      end: "bottom bottom",
+      scrub: true,
+    }
+  });
+
+  gsap.to(camera.rotation, {
+    immediateRender: false,
+    y: 0,
+    scrollTrigger: {
+      trigger: '#first-section-ending',
+      start: "top bottom",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
+
+  gsap.fromTo('.threeContainer', 
+  {
+    immediateRender: false,
+    top: 0
+  },
+  {
+    position: 'absolute',
+    top: 'calc(100% - 100vh)',
+    scrollTrigger: {
+      trigger: '#first-section-ending',
+      start: "bottom bottom",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
+}
+
 function onScroll(e: Event) {
   if (window.location.pathname === '/work' && document.documentElement.scrollTop - parseInt(sessionStorage.getItem('work-scrollPosition') ?? '0') < 100)
       sessionStorage.setItem('work-scrollPosition', document.documentElement.scrollTop.toString());
 }
 
-function enterThree(elementQuery: string, scene: THREE.Scene, object: CSS3DObject) {
+function setThreeEnter(elementQuery: string, scene: THREE.Scene, object: CSS3DObject) {
   gsap.fromTo('.threeContainer', 
   {
     position: 'absolute',
