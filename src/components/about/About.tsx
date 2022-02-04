8af517f79a212fe7a69b8767096082033b8b6555
dandeduck <danielmkhlv@gmail.com>
2022-02-04 14:11:47 +0200

    Added CV

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -36,7 +36,7 @@ export default function About() {
         <p><span className='know'>Frameworks and libraries: </span>Angular 1 year, React 1 year, Express 1 year, NestJS 1 year, Pytorch 1 year</p>
         <p><span className='know'>DBs and technologies: </span>Linux 4 years, MongoDB 1 year, Redis 1 year, Node 1 year, Firebase 1 year</p>
         <h1>Experience</h1>
-        <Experience company='israeli air force' title='fullstack developer' from={'April 2021'} to={'April 2022'} points={['Developed webapps in use by thousands of users', 'Integrate complex, multi interface and legacy programs', 'Used the MEAN stack with Azure for most projects']}/>
+        <Experience company='israeli air force' title='fullstack developer' from={'April 2021'} to={'April 2022'} points={['Developed webapps in use by thousands of users', 'Integrated complex, multi interface and legacy programs', 'Used the MEAN stack with Azure for most projects']}/>
         <Experience company='FRC team' title='lead programmer' from={'July 2017'} to={'July 2020'} points={['Led a team of 6 developers, from teaching and mentoring to reviewing and delegating tasks', 'Worked on complex systems as part of a large 60kg robot', 'Created and contributed to many open source projects in Java', 'Setup an efficient and organized workflow to maximise productivity']}/>
         <h1>Education</h1>
         <Experience company='ort rabin' title='computer science diploma' from={'September 2017'} to={'July 2020'} points={[]}/>
@@ -47,7 +47,7 @@ export default function About() {
         <Experience company='This site' title='Front end developer' from={'January 2022'} to={'February 2022'} points={['Built using React, including Three.js, Firebase and gsap']}/>
         <Experience company='Flash 2020' title='Lead developer' from={'January 2020'} to={'March 2020'} points={['Code for the 2020 Flash FRC robot', 'Implemented autonomous movement with ball shooting using vision processing and speed interpolation', 'Collaborated with many other developers on the main systems of the robot']}/>
         <h2>To see more of my projects: go to</h2> <Link to={'/work'} className='link'>my work</Link> <h2>or</h2> <a href='https://github.com/dandeduck' className='link'>my github</a>
-        <div><h2>For my CV: <Link to="/logo192.png" className='link' target="_blank" download>download</Link></h2></div>
+        <div><h2>For my CV: <Link to="/Daniel_Mikhailov.pdf" className='link' target="_blank" download>download</Link></h2></div>
       </section>
     </div>
   );
